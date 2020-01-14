71f2441921e7305c8891fb78790733effb573628
Zefirrat <leonidpavlov314@gmail.com>
2020-01-15 03:03:19 +0500

    Ooooops.. Accidentally crated Abstract Factory instead of Factory Method..

---
+++ C#/Patterns/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    #region Factory
+
+    public abstract class Shop
+    {
+        public abstract Cookies BuyCookies();
+    }
+
+    public class Amazon : Shop
+    {
+        public override Cookies BuyCookies()
+        {
+            return new ChocoPie();
+        }
+    }
+
+    public class Wallmart : Shop
+    {
+        public override Cookies BuyCookies()
+        {
+            return new Oreo();
+        }
+    }
+
+    #endregion
+
+    #region Product
+
+    public abstract class Cookies
+    {
+        public string Name;
+        public bool IsYummy;
+
+        public void Eat()
+        {
+            string message;
+            message = $"Eating {Name}.";
+            if (IsYummy)
+                message += " Yummy!";
+            else
+                message += " You are a fool if you love it...";
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Oreo : Cookies
+    {
+        public Oreo()
+        {
+            Name = "Oreo";
+            IsYummy = false;
+        }
+    }
+
+    public class ChocoPie : Cookies
+    {
+        public ChocoPie()
+        {
+            Name = "ChocoPie";
+            IsYummy = true;
+        }
+    }
+
+    #endregion
+}
